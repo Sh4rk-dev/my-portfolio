75a54a4b66d3e4fe72996e35bb398e3e57523344
Renan <renanrapace13@gmail.com>
2024-04-25 16:44:04 -0300

    Creating the array of objects in the moked folder

---
+++ src/moked/ESTUDOS.ts
@@ -0,0 +1,60 @@
+import { IStudys } from "@/lib/interface";
+
+const orientation = {
+  left: "order-0 lg:-order-1",
+  right: "order-1 lg:order-2",
+};
+
+const variantsColors = {
+  blue: "bg-blue-600",
+  green: "bg-emerald-500",
+  orange: "bg-_orange",
+};
+
+const variantsTexts = {
+  textBlue: "bg-blue-100 text-blue-600",
+  textGreen: "bg-emerald-100 text-emerald-500",
+  textOrange: "bg-_orangeBackGround text-_orange",
+};
+
+export const studys: IStudys[] = [
+  {
+    id: 1,
+    tag: "Mobile",
+    title: "NLW-Unite-Mobile",
+    color: variantsColors.orange,
+    orientation: orientation.right,
+    colorText: variantsTexts.textOrange,
+    buttonLink: "https://github.com/Sh4rk-dev/NLW-Unite-Mobile",
+    description:
+      "Desenvolvimento de uma aplicação mobile em React Native, aplicação dos conceitos de Propriedades, Estados e Componentes, tipagem com Typescript, Expo Framework, interface com Native Wind, gerenciamento de estado global com Zustand, roteamento por arquivos com Expo Router, consumo de API Node.js, animações com Moti.",
+    image:
+      "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    tag: "Website",
+    title: "NLW-Unite-React",
+    color: variantsColors.blue,
+    orientation: orientation.left,
+    colorText: variantsTexts.textBlue,
+    buttonLink: "https://github.com/Sh4rk-dev/NLW-Unite-React",
+    description:
+      "Desenvolvimento de uma aplicação front-end em ReactJS, aplicação dos conceitos de Propriedades, Estados e Componentes, tipagem com Typescript, tooling com Vite, interface responsiva com TailwindCSS, consumo de API Node.js, uso de URL states.",
+    image:
+      "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    tag: "Backend",
+    title: "NLW-Unite-NodeJS",
+    color: variantsColors.green,
+    orientation: orientation.right,
+    colorText: variantsTexts.textGreen,
+    buttonLink: "https://github.com/Sh4rk-dev/NLW-Unite-NodeJS",
+    description:
+      "Desenvolvimento de uma aplicação back-end em Node.js, aplicação dos conceitos de API REST, utilizando TypeScript, Fastify como framework, integração do Prisma ORM + SQLite e Zod para validação de dados.",
+    image:
+      "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
