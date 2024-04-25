adb0bf42ca1a6915b4bb7d65f503704ae5f0910e
Renan <renanrapace13@gmail.com>
2024-04-25 16:50:33 -0300

    New interface (Studies) and updating the object array (FeedBacks)

--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -5,3 +5,15 @@ export interface IFeedBack {
   feedback: string;
   profession: string;
 }
+
+export interface IStudys {
+  id: number;
+  tag: string;
+  color: string;
+  title: string;
+  image?: string;
+  colorText: string;
+  buttonLink: string;
+  orientation: string;
+  description: string;
+}
