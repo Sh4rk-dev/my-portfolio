19c76623a161b8f34041e51b87f8a7a52200153e
Renan <renanrapace13@gmail.com>
2024-05-03 15:37:08 -0300

    Readjustment: Fixing resume download link

--- src/components/selectModal.tsx
+++ src/components/selectModal.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 export function SelectModal() {
   return (
     <div className="flex justify-center md:justify-end gap-5">
-      <a href="https://drive.google.com/uc?export=download&id=1bbsdsvLVBD62WRQ02JW3W0rgaTZAem82">
+      <a href="https://drive.google.com/file/d/13X0WyldDrq6iolKiXVPdfozjV9ZaUwdr/view?usp=sharing">
         <Button className="w-20 bg-lime-500 text-white">PDF</Button>
       </a>
 
